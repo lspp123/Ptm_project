41008dd1055b6ada4f4432f8a838d46871498712
lspp123 <spector.liram3@gmail.com>
2025-01-11 18:45:57 +0200

    Implementation of Topic

--- src/graph/Topic.java
+++ src/graph/Topic.java
@@ -5,23 +5,31 @@ import java.util.List;
 
 public class Topic {
     public final String name;
+    ArrayList<Agent> subs;
+    ArrayList<Agent> pubs;
 
     Topic(String name){
         this.name=name;
     }
 
     public void subscribe(Agent a){
+        subs.add(a);
     }
+
     public void unsubscribe(Agent a){
+        subs.remove(a);
     }
 
     public void publish(Message m){
+        pubs.forEach(a -> a.callback(this.name, m));
     }
 
     public void addPublisher(Agent a){
+        pubs.add(a);
     }
 
     public void removePublisher(Agent a){
+        pubs.remove(a);
     }
 
 

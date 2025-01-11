accb073b884b2a8d001ee8e1151ec7619077b91e
lspp123 <spector.liram3@gmail.com>
2025-01-11 16:46:23 +0200

    Constructor definition for Message

--- src/graph/Message.java
+++ src/graph/Message.java
@@ -8,5 +8,18 @@ public class Message {
     public final double asDouble;
     public final Date date;
 
-    
+    public Message(String message_text) {
+        this.data = message_text.getBytes().clone();
+        this.asText = message_text;
+        this.asDouble = parseDoubleOrNaN(message_text);
+        this.date = Date.from(java.time.Instant.now());
+    }
+
+    private double parseDoubleOrNaN(String input) {
+        try {
+            return Double.parseDouble(input);
+        } catch (NumberFormatException e) {
+            return Double.NaN;
+        }
+    }
 }

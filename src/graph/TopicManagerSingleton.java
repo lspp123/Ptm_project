334b0a43e8b7347d74a4470fbf72be8e921bc698
lspp123 <spector.liram3@gmail.com>
2025-01-11 19:50:28 +0200

    Implementation of TopicManagerSingleton

--- src/graph/TopicManagerSingleton.java
+++ src/graph/TopicManagerSingleton.java
@@ -1,10 +1,37 @@
 package graph;
 
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class TopicManagerSingleton {
 
     public static class TopicManager{
+        private static final TopicManager instance = new TopicManager();
+        Map<String, Topic> topics;
+
+        private TopicManager() {
+            topics = new ConcurrentHashMap<String, Topic>();
+        }
+
+        public Topic getTopics(String topicName) {
+            if (!topics.containsKey(topicName))
+                topics.put(topicName, new Topic(topicName));
+            return topics.get(topicName);
+        }
+
+        public Collection<Topic> getTopics() {
+            return topics.values();
+        }
+
+        public void clear() {
+            topics.clear();
+        }
+
     }
 
-    
+    public static TopicManager get() {
+        return TopicManager.instance;
+    }
+
 }
